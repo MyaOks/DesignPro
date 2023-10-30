c61fa185aadcc58ba3dd4a151733ba149aa2a4a7
MyaOks <oksana.myasnikova.2003@mail.ru>
2023-10-30 14:37:12 +0700

    Создание модели пользователя, базового шаблона и шаблона главной страницы

--- DesignPro/settings.py
+++ DesignPro/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'interiors.apps.InteriorsConfig',
-    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
--- interiors/models.py
+++ interiors/models.py
@@ -7,12 +7,12 @@ class AdvUser(AbstractUser):
         ('user', 'Пользователь'),
         ('guest', 'Гость')
     ]
-    first_name = models.CharField(max_length=254, verbose_name='Имя', blank=False)
-    last_name = models.CharField(max_length=254, verbose_name='Фамилия', blank=False)
+    first_name = models.CharField(max_length=254, verbose_name='Имя', blank=False, default='')
+    last_name = models.CharField(max_length=254, verbose_name='Фамилия', blank=False, default='')
     patronymic = models.CharField(max_length=254, verbose_name='Отчество', blank=True)
-    username = models.CharField(max_length=254, verbose_name='Логин', unique=True, blank=False)
-    email = models.CharField(max_length=254, verbose_name='Почта', unique=True, blank=False)
-    password = models.CharField(max_length=254, verbose_name='Пароль', blank=False)
+    username = models.CharField(max_length=254, verbose_name='Логин', unique=True, blank=False, default='')
+    email = models.CharField(max_length=254, verbose_name='Почта', unique=True, blank=False, default='')
+    password = models.CharField(max_length=254, verbose_name='Пароль', blank=False, default='')
     role = models.CharField(max_length=254, verbose_name='Роль', choices=ROLE_CHOICES, default='user')
 
     USERNAME_FIELD = 'username'
